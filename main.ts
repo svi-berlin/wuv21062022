8a2ee9792f44d668bc6c7c0a77444fd1a14adaf0
svi-berlin <svi-berlin@users.noreply.github.com>
2022-06-21 14:58:47 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 function alarm () {
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.setLedColor(0x0000ff)
+    serialmp3.playMp3Folder(1, Mp3Repeat.No)
+    basic.pause(2000)
     basic.setLedColor(0xff0000)
     servo = randint(20, 150)
     pins.servoWritePin(AnalogPin.P1, servo)
@@ -7,8 +9,10 @@ function alarm () {
 }
 let mikro = 0
 let servo = 0
+serialmp3.connectSerialMp3(DigitalPin.C16, DigitalPin.C17)
+serialmp3.setMp3Volume(30)
 servo = 0
-let grenzwert = 530
+let grenzwert = 520
 basic.forever(function () {
     mikro = pins.analogReadPin(AnalogPin.MIC)
     basic.showNumber(mikro)
